4dbd2030a687bd77eec967f6e4b1da5f5e612021
ccw7463 <changwoo7463@gmail.com>
2022-07-15 08:07:41 +0900

    update

--- 알고리즘기초 1/자료구조1/문제4_스택수열.py
+++ 알고리즘기초 1/자료구조1/문제4_스택수열.py
@@ -1,17 +1,24 @@
-NUM = int(input())
-num_string = [i+1 for i in range(NUM)]
+n = int(input())
 stack = []
-for i in range(NUM):
-    num = int(input())
-    
-    if stack and stack[-1]==num:
-        stack.pop()
-        continue
+answer = []
+flag = 0
+cur = 1
 
-    
-    for j in num_string:
-        stack.append(j+1)
-    
-    if stack and stack[-1]==num:
+for i in range(n):
+    num = int(input())
+    while cur<=num:
+        stack.append(cur)
+        answer.append("+")
+        cur+=1
+
+    if stack[-1]==num:
         stack.pop()
-    
+        answer.append("-")
+    else:
+        print("NO")
+        flag=1
+        break
+
+if flag==0:
+    for i in answer:
+        print(i)
