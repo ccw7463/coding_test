c985f2378d55bfab27aab5f414bea4cb0b6d2b99
창우의 맥북 <changwoo@changuucBookPro.s4.islab.re.kr>
2022-07-27 12:09:34 +0900

    update

--- 알고리즘기초 1/자료구조1/문제5_에디터.py
+++ 알고리즘기초 1/자료구조1/문제5_에디터.py
@@ -1,36 +1,9 @@
-from collections import deque
 
+# L 커서 왼쪽
+# D 커서 오른쪽
+# B 커서 왼쪽 문자 삭제
+# P $ 커서 왼쪽에 문자추가
+# input = ['abcd',3,'P x','L','P y']
+# inp = ['abc',9,'L','L','L','L','L','P x','L','B','P y']
 
-# in_str = deque([i for i in input()])
-in_str = list(input())
-in_str = in_str + ["cur"]
-in_str = deque(in_str)
-num = int(input())
-
-for _ in range(num):
-    command = input()
-    if command.startswith("L"):
-        if in_str[0]=="cur":
-            pass
-        else:
-            in_str.rotate(-1)
-            in_str.appendleft(in_str.pop())
-        continue
-    if command.startswith("D"):
-        if in_str[-1]=="cur":
-            pass
-        else:
-            in_str.rotate(1)
-            in_str.append(in_str.popleft())
-        continue
-    if command.startswith("B"):
-        idx = in_str.index("cur")
-        in_str = in_str[:(idx-1)] + in_str[idx:]
-        continue
-    if command.startswith("P"):
-        insert_str = command.split(" ")[-1]
-        idx = in_str.index("cur")
-        in_str = in_str[:idx]+[insert_str]+in_str[idx:]
-        continue
-answer = ''.join(in_str)
-print(answer)
+# 시간복잡도때문에 스택두개를 사용
